5243de64de4dadebbb6a3f77bb2fe4372f8e8fbf
afsvn-exe <afsvnexe@proton.me>
2026-01-15 00:11:40 -0500

    Day-12: bullet-proof string copy

---
+++ season-1-foundation/day-12-str-ops-buff-defense/day12_recovery.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stddef.h> 
+
+size_t safe_copy(char *dest, const char *src, size_t dest_size){
+	
+	if(dest == 0){
+	   return 0;
+	}
+	
+	size_t i = 0; 
+	while(src[i] != '\0' && i < dest_size - 1){
+	    dest[i] = src[i]; 
+	    i++; 
+	}
+	dest[i] = '\0'; 
+
+	return i; 
+}
+
+int main(void){
+	char buffer[8]; 
+	size_t copied; 
+
+	// Test 1: Normal Copy
+	copied = safe_copy(buffer, "hello", 8); 
+	printf("Test 1: \"%s\" (copied %zu)\n", buffer, copied);
+	
+	// Test 2: Truncation 
+	copied = safe_copy(buffer, "Hello, World!!", 8);
+	printf("Test 2: \"%s\" (copied %zu)\n", buffer, copied);
+
+	// Test 3: Empty source 
+	copied = safe_copy(buffer, "", 8);
+        printf("Test 3: \"%s\" (copied %zu)\n", buffer, copied);
+
+	return 0; 	
+}
